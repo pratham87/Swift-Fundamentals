6486e7e814c701157faf40ca20f40d01d4b01c7a
Prathamesh Kawale <kawa5@live.com>
2016-12-29 13:40:42 -0800

    math operations

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -22,3 +22,15 @@ print("Casting Int value: \(Float(test1))")
 var age: String = "15"
 print("Casting to Int: \(Int(age))")
 print("Casting to float: \((age as NSString).floatValue)")
+
+
+/****************************************** Math **********************************************/
+print(2 + 2)
+print(3 % 2)
+
+//++ -- deprecated in swift 3
+var num = 1
+print(num + 1)
+
+//Random number generator. Generates random number between 0 - 19
+print("Random number is: \(arc4random() % 20)")
