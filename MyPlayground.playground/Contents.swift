56b8846b3fffe8e3f622825f024fd5e05bfd6fa5
Prathamesh Kawale <kawa5@live.com>
2017-01-18 16:35:28 -0800

    generics and extensions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -342,17 +342,35 @@ print(favColor.rawValue)
 print(favColor.getColor())
 
 
+/********************************************* Generics ************************************************/
+
+func printAnyArray<T>(anything: [T]){
+    for element in anything{
+        print("\(element), ")
+    }
+}
+
+printAnyArray(anything: ["Paul", "Jake"])
+printAnyArray(anything: [1, 2, 3])
 
 
+/********************************************* Extensions ************************************************/
 
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
 
+// Prints "One inch is 0.0254 meters"
+let oneInch = 25.4.mm
+print("One inch is \(oneInch) meters")
 
-
-
-
-
-
-
+// Prints "Three feet is 0.914399970739201 meters"
+let threeFeet = 3.ft
+print("Three feet is \(threeFeet) meters")
 
 
 
